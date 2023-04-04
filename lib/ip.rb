ade8da70300e3cd101396df28d4c39e21b0a925c
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-04-04 08:53:39 -0300

    final solution

--- lib/ip.rb
+++ lib/ip.rb
@@ -1,5 +1,6 @@
 def ip_to_num(ip_address)
   # TODO: return the number version of the `ip_address` string
+
 array = []
 ip_array = []
 bits_array = [128, 64, 32, 16, 8, 4, 2, 1,]
@@ -42,8 +43,77 @@ binary_array.each.with_index do |current, index|
     final_number += 2 ** index
   end
 end
+
+return final_number
+
 end
 
 def num_to_ip(number)
   # TODO: return the string IP address from the `number`
+
+final_number = number
+binary_array = []
+bits_array = [128, 64, 32, 16, 8, 4, 2, 1,]
+sum = 0
+current_number = ""
+ip_array = []
+
+# An array to do the potentiation
+array = (0..31).reverse_each.to_a
+
+# Tranform the number in binary
+array.each do |c|
+  if final_number < 2 ** c
+    binary_array << 0
+  elsif final_number >= 2 ** c
+    binary_array << 1
+    final_number -= 2 ** c
+  elsif final_number == 0
+    binary_array << 0
+    end
 end
+
+# Separete the binary number in 4 blocks of 8 characteres
+binary_bits_array_of_arrays = binary_array.each_slice(8).to_a
+
+# Transform the Binary number in an ip number
+binary_bits_array_of_arrays.each do |arra|
+  if current_number.length <= 0
+    current_number = arra.join
+    current_number.each_char.with_index do |c, i|
+      bits_array.each.with_index do |current, index|
+        if c == "1" && i == index
+          sum += current
+        end
+        if i == 7 && index == 7
+          ip_array << sum
+          sum = 0
+          current_number = ""
+        end
+      end
+    end
+  end
+end
+
+ip = ip_array.join(".")
+
+
+return ip
+end
+
+
+# Tests
+puts ip_to_num("37.160.113.170") # Should return 631271850
+puts num_to_ip(631271850) # Should return "37.160.113.170"
+
+puts ip_to_num("192.168.1.100") # Should return 3232235876
+puts num_to_ip(3232235876) # Should return "192.168.1.100"
+
+puts ip_to_num("10.0.0.2") # Should return 167772162
+puts num_to_ip(167772162) # Should return "10.0.0.2"
+
+puts ip_to_num("172.16.254.1") # Should return 167772162
+puts num_to_ip(2886794753) # Should return "172.16.254.1"
+
+puts ip_to_num("8.8.8.8") # Should return 134744072
+puts num_to_ip(134744072) # Should return "8.8.8.8"
